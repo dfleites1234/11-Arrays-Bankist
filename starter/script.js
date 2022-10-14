c775ad1b8b9c3455d0cdb4e00ba9e3e0a56c7e79
Sparrow <dfleites1234@gmail.com>
2022-10-14 14:01:24 -0500

    The reduce Method
    Adding up the balance and logging it to the website...
    
    Using reduce method to find the maximum value of deposits out of an array.

--- starter/script.js
+++ starter/script.js
@@ -1,5 +1,5 @@
 'use strict';
-/*
+
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // BANKIST APP
@@ -80,6 +80,12 @@ const displayMovements = function (movements) {
 };
 displayMovements(account1.movements);
 
+const calcDisplayBalance = function (movements) {
+  const balance = movements.reduce((acc, mov) => acc + mov, 0);
+  labelBalance.textContent = `${balance} EUR`;
+};
+calcDisplayBalance(account1.movements);
+
 const createUsernames = function (accs) {
   accs.forEach(function (acc) {
     acc.username = acc.owner
@@ -89,13 +95,12 @@ const createUsernames = function (accs) {
       .join('');
   });
 };
-
 createUsernames(accounts);
-console.log(accounts);
 
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
+/*
 
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
@@ -261,8 +266,7 @@ const movementsDescriptions = movements.map(
     )}`
 );
 console.log(movementsDescriptions);
-*/
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // The filter Method
 
@@ -278,3 +282,25 @@ console.log(depositsFor);
 
 const withdrawals = movements.filter(mov => mov < 0);
 console.log(withdrawals);
+
+*/
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// The reduce Method
+
+console.log(movements);
+
+// accumulator -> SNOWBAll
+const balance = movements.reduce((acc, cur) => acc + cur, 0);
+console.log(balance);
+
+let balance2 = 0;
+for (const mov of movements) balance2 += mov;
+console.log(balance2);
+
+// Maximum value
+const max = movements.reduce((acc, mov) => {
+  if (acc > mov) return acc;
+  else return mov;
+}, movements[0]);
+console.log(max);
